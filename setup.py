ddd5f87342717ca7fdfda041ad338a60f269f329
Zw Pan <pzweuj@live.com>
2023-10-22 20:34:34 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# pzw
+# 20231009
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(name='WordWriter',
+    version = '3.0.3',
+    description = 'Docx file template replacing',
+    long_description = long_description,
+    long_description_content_type =' text/markdown',
+    author = 'pzweuj',
+    author_email = 'pzweuj@live.com',
+    url = 'https://github.com/pzweuj/WordWriter',
+    install_requires = ["python-docx", "pandas"],
+    license = 'MIT License',
+    packages = find_packages(),
+    platforms = ["all"],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
