825d799ff634212a1c816702393b8ed88d9faa14
EuJ <pzweuj@live.com>
2023-11-24 13:54:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setup(name='WordWriter',
-    version = '3.2.0',
+    version = '3.2.1',
     description = 'Docx file template replacing',
     long_description = long_description,
     long_description_content_type =' text/markdown',
